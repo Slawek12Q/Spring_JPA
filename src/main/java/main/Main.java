f14b9c9e7c137d7a158b84819a3e66febaf4ac09
Slawek12Q <slawek.guzik@interia.pl>
2019-01-26 23:15:32 +0100

    Spring configuration v3_1

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,14 +3,20 @@ package main;
 import DAO.BookDao;
 import DAO.BookDaoImpl;
 import model.Book;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        Book book = new Book("123456789", "Wilk z Wall Street", "Jordan Belfort");
-        BookDao bookDao = new BookDaoImpl();
+        AnnotationConfigApplicationContext ctx =
+                new AnnotationConfigApplicationContext(Main.class);
+        BookDao bookDao = ctx.getBean(BookDao.class);
+
+        Book book = new Book("1234567890468", "Spring is so cool", "Slawoir");
         bookDao.save(book);
-        System.out.println("Book saved");
-        bookDao.cleanUp();
+
+        Book bookGet = bookDao.get(2L);
+        System.out.println(bookGet);
+        ctx.close();
     }
 }
