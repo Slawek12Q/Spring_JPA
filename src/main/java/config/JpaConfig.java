12708c93d997f5192bb24d392e739dbd6ef59197
Slawek12Q <slawek.guzik@interia.pl>
2019-01-26 23:02:13 +0100

    spring configuration v2

--- src/main/java/config/JpaConfig.java
+++ src/main/java/config/JpaConfig.java
@@ -1,4 +1,15 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+
+@Configuration
 public class JpaConfig {
+    @Bean
+    public LocalEntityManagerFactoryBean createEMF() {
+        LocalEntityManagerFactoryBean emf = new LocalEntityManagerFactoryBean();
+        emf.setPersistenceUnitName("spring-jpa-pu");
+        return emf;
+    }
 }
