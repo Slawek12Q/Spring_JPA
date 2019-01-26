304dd339f86ada5fd01522ef53a75af155f8774d
Slawek12Q <slawek.guzik@interia.pl>
2019-01-26 23:08:30 +0100

    Spring configuration v3

--- src/main/java/DAO/BookDaoImpl.java
+++ src/main/java/DAO/BookDaoImpl.java
@@ -1,40 +1,38 @@
 package DAO;
 
 import model.Book;
+import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 
+@Repository
 public class BookDaoImpl implements BookDao {
 
+    @PersistenceUnit
     private EntityManagerFactory emFactory;
-    private EntityManager entityManager;
 
-    public BookDaoImpl() {
-        emFactory = Persistence.createEntityManagerFactory("myPersistenceUnit");
-        entityManager = emFactory.createEntityManager();
+
+    BookDaoImpl(){
+
     }
 
     @Override
     public void save(Book book) {
+        EntityManager entityManager = emFactory.createEntityManager();
         EntityTransaction tx = entityManager.getTransaction();
         tx.begin();
         entityManager.persist(book);
         tx.commit();
+        entityManager.close();
     }
 
     @Override
     public Book get(Long id) {
+        EntityManager entityManager = emFactory.createEntityManager();
         Book book = entityManager.find(Book.class, id);
+        entityManager.close();
         return book;
     }
 
-    @Override
-    public void cleanUp() {
-        entityManager.close();
-        emFactory.close();
-    }
 }
